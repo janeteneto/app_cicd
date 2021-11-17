1f46c146449b2ee58a2d63d879c88a8d645eb7d6
A. H Shahrukh Khan <50804801+khanmaster@users.noreply.github.com>
2021-11-17 17:00:56 +0000

    Update provision.sh

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -21,8 +21,9 @@ sudo npm install pm2 -g
 sudo apt-get install nginx -y
 
 # remove the old file and add our one
-sudo rm /etc/nginx/sites-available/default
-sudo cp /home/ubuntu/sre_jenkins_cicd/environment/app/nginx.default /etc/nginx/sites-available/default
+#sudo rm /etc/nginx/sites-available/default
+#sudo cp /home/ubuntu/sre_jenkins_cicd/environment/app/nginx.default /etc/nginx/sites-available/default
 
 # finally, restart the nginx service so the new config takes hold
-sudo service nginx restart
+#sudo service nginx restart
+#sudo service nginx enable
