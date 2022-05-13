cc6744823f6d7a2ec2cabed5c34f246282cb5615
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2022-05-13 12:26:07 +0100

    ci heading added

--- app/test/test-server.js
+++ app/test/test-server.js
@@ -15,11 +15,11 @@ describe('Homepage', function() {
         done();
       });
   });
-  it('should contain the word testing at / GET', function(done) {
+  it('should contain the word Sparta at / GET', function(done) {
     chai.request(server)
       .get('/')
       .end(function(err, res){
-        res.text.should.contain('test')
+        res.text.should.contain('Sparta')
         done();
       });
   });
